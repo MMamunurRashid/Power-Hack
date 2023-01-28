776f3ec6475eef48d499805b989a0e0418332acc
Mamunur-Rashid <mdmamun.iubat.m@gmail.com>
2023-01-29 01:59:49 +0600

    Setup foolder structure

---
+++ src/Shared/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-200">
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
